2507e8364d790e01d2a5ac76312fe8ec4e7cade0
nicolaciar <nicolas.laciar@mi.unc.edu.ar>
2025-12-01 14:16:07 -0300

    lab del practico 0

--- Practicos/p0.hs
+++ Practicos/p0.hs
@@ -60,3 +60,57 @@ todosIguales :: [Int] -> Bool
 todosIguales [] = True
 todosIguales [_] = True
 todosIguales (x:y:xs) = (x==y) && todosIguales (y:xs)
+
+
+-- Figuras
+
+data Color = Rojo | Amarillo | Azul | Verde 
+                        deriving (Show, Eq)
+data Forma = Triangulo | Cuadrado | Rombo | Circulo
+                        deriving (Show, Eq)
+type Figura = (Forma, Color, Int)
+
+
+--- Laboratorio 4
+
+rojo :: Figura -> Bool
+rojo (_,c,_) = c == Rojo
+
+amarillo :: Figura -> Bool
+amarillo (_, c, _) = c == Amarillo
+
+azul :: Figura -> Bool
+azul (_, c, _) = c == Azul
+
+verde :: Figura -> Bool
+verde (_,c,_) = c==Verde
+
+tam :: Figura -> Int
+tam (_,_,c) = c
+
+triangulo :: Figura -> Bool
+triangulo (c,_,_) = c==Triangulo
+
+cuadrado :: Figura -> Bool
+cuadrado (c,_,_) = c==Cuadrado
+
+rombo :: Figura -> Bool
+rombo (c,_,_) = c==Rombo
+
+circulo :: Figura -> Bool
+circulo (c,_,_) = c==Circulo
+
+
+-- Laboratorio 5
+xs1, xs2, xs3 :: [Figura]
+xs1 = [(Circulo, Azul, 40), (Rombo, Rojo, 10)]
+xs2 = [(Circulo, Rojo, 4), (Rombo, Rojo, 10)]
+xs3 = [(Triangulo, Verde, 10)]  -- Listas a evaluar
+
+todasRojas :: [Figura] -> Bool
+todasRojas [] = True
+todasRojas (x:xs) = rojo x && todasRojas xs
+
+todasRombo :: [Figura] -> Bool
+todasRombo [] = True
+todasRombo (x:xs) = rombo x && todasRombo xs
